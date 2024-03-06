20994d60521ac58d448111d2dab90c003c1169b6
Eduard <sekaneduard@gmail.com>
2024-03-06 18:49:07 +0200

    add features header / mobile

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// dropdownContent
+
 document.addEventListener('DOMContentLoaded', () => {
   const dropdownWrap = document.getElementById('dropdownWrap');
   const dropdownContent = document.getElementById('dropdownContent');
@@ -16,11 +18,49 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// Hamburger
 document.addEventListener('DOMContentLoaded', function () {
   const hamburgerIcon = document.querySelector('.hamburger-icon');
-  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const closeIcon = document.querySelector('.close-icon');
+  const mobileMenu = document.querySelector('.mobile-menu');
 
+  // Обработчик клика по иконке бургера
   hamburgerIcon.addEventListener('click', function () {
-    hamburgerMenu.classList.toggle('show');
+    // Переключаем отображение иконок бургера и крестика
+    hamburgerIcon.style.display = 'none';
+    closeIcon.style.display = 'block';
+    // Показываем меню
+    mobileMenu.classList.add('open');
+  });
+
+  // Обработчик клика по иконке крестика
+  closeIcon.addEventListener('click', function () {
+    // Переключаем отображение иконок крестика и бургера
+    closeIcon.style.display = 'none';
+    hamburgerIcon.style.display = 'block';
+    // Скрываем меню
+    mobileMenu.classList.remove('open');
+  });
+});
+
+// Popup
+
+document.addEventListener('DOMContentLoaded', function () {
+  const mobileSvg = document.querySelector('.mobile-svg');
+  const phonePopup = document.querySelector('.phone-popup-mobile');
+
+  mobileSvg.addEventListener('click', function () {
+    phonePopup.style.display =
+      phonePopup.style.display === 'none' ? 'block' : 'none';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const popup = document.querySelector('.phone-popup');
+  const contentPopup = document.querySelector('.form-popup');
+
+  popup.addEventListener('click', function () {
+    contentPopup.style.display =
+      contentPopup.style.display === 'none' ? 'block' : 'none';
   });
 });
