62dc3aacc94835f22cf97052f829f44d09a85c83
Eduard <sekaneduard@gmail.com>
2024-03-08 00:18:50 +0200

    add new section why-choose-us and fix header before add form

--- src/index.js
+++ src/index.js
@@ -24,21 +24,21 @@ document.addEventListener('DOMContentLoaded', function () {
   const closeIcon = document.querySelector('.close-icon');
   const mobileMenu = document.querySelector('.mobile-menu');
 
-  // Обработчик клика по иконке бургера
+  // Burger icon click handler
   hamburgerIcon.addEventListener('click', function () {
-    // Переключаем отображение иконок бургера и крестика
+    // Switch the display of the burger and cross icons
     hamburgerIcon.style.display = 'none';
     closeIcon.style.display = 'block';
-    // Показываем меню
+    // Show menu
     mobileMenu.classList.add('open');
   });
 
-  // Обработчик клика по иконке крестика
+  // Click handler on the cross icon
   closeIcon.addEventListener('click', function () {
-    // Переключаем отображение иконок крестика и бургера
+    // Switch the display of the cross and burger icons
     closeIcon.style.display = 'none';
     hamburgerIcon.style.display = 'block';
-    // Скрываем меню
+    // Hide the menu
     mobileMenu.classList.remove('open');
   });
 });
@@ -55,12 +55,23 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+// Form-Popup
+
 document.addEventListener('DOMContentLoaded', function () {
-  const popup = document.querySelector('.phone-popup');
+  const popup = document.querySelector('.phone-popup-form-popup');
   const contentPopup = document.querySelector('.form-popup');
+  const formCloseIcon = document.querySelector('.form-close-icon');
+  const phonePopup = document.querySelector('.phone-popup-mobile');
 
   popup.addEventListener('click', function () {
     contentPopup.style.display =
       contentPopup.style.display === 'none' ? 'block' : 'none';
+      phonePopup.style.display === 'none'
+  });
+
+  formCloseIcon.addEventListener('click', function () {
+    contentPopup.style.display = 'none';
+    phonePopup.style.display = 'none';
   });
 });
+
