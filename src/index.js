50bfa99ff7c32a0422707f8d7da9e73a5d5997f6
Eduard <sekaneduard@gmail.com>
2024-04-30 18:43:24 +0300

    fix swiper

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import './js/dropdown.js';
 import './js/hamburger.js';
 import './js/popup.js';
 import './js/formPopup.js';
-import './js/swiper.js';
 import './js/currentLink.js';
+import './js/swiper.js';
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,69 +1,136 @@
 document.addEventListener('DOMContentLoaded', function () {
   const isMobileOrTablet = window.innerWidth <= 1440;
   if (isMobileOrTablet) {
-    const swiper1 = new Swiper('.swiper-container', {
-      slidesPerView: 'auto',
-      spaceBetween: 20,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
-        732: {
-          slidesPerView: 2,
-          spaceBetween: 20,
+    const swiperData = [
+      {
+        container: '.swiper-container',
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        breakpoints: {
+          732: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
         },
       },
-    });
+      {
+        container: '.swiper-container1',
+        pagination: '.swiper-pagination1',
+        nextButton: '.swiper-button-next1',
+        prevButton: '.swiper-button-prev1',
+        breakpoints: {
+          732: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+        },
+      },
+      {
+        container: '.swiper-container2',
+        pagination: '.swiper-pagination2',
+        nextButton: '.swiper-button-next2',
+        prevButton: '.swiper-button-prev2',
+        breakpoints: {
+          732: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+        },
+      },
+      {
+        container: '.swiper-container3',
+        pagination: '.swiper-pagination3',
+        nextButton: '.swiper-button-next3',
+        prevButton: '.swiper-button-prev3',
+        breakpoints: {
+          732: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+        },
+      },
+      {
+        container: '.swiper-container4',
+        pagination: '.swiper-pagination4',
+        nextButton: '.swiper-button-next4',
+        prevButton: '.swiper-button-prev4',
+        breakpoints: {
+          732: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+        },
+      },
+      {
+        container: '.swiper-container5',
+        pagination: '.swiper-pagination5',
+        nextButton: '.swiper-button-next5',
+        prevButton: '.swiper-button-prev5',
+        breakpoints: {
+          732: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+        },
+      },
+      {
+        container: '.swiper-container6',
+        pagination: '.swiper-pagination6',
+        nextButton: '.swiper-button-next6',
+        prevButton: '.swiper-button-prev6',
+        breakpoints: {
+          732: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+        },
+      },
+    ];
 
-    const swiper = new Swiper('.swiper-container2', {
-      slidesPerView: 'auto', // розділити на слайди
-      spaceBetween: 20,
-      pagination: {
-        el: '.swiper-pagination2',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next2',
-        prevEl: '.swiper-button-prev2',
-      },
-      breakpoints: {
-        732: {
-          slidesPerView: 2,
-          spaceBetween: 20,
+    swiperData.forEach(data => {
+      new Swiper(data.container, {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        loop: true,
+        pagination: {
+          el: data.pagination,
+          clickable: true,
         },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 20,
+        navigation: {
+          nextEl: data.nextButton,
+          prevEl: data.prevButton,
         },
-      },
-    });
-
-    const swiper3 = new Swiper('.swiper-container3', {
-      slidesPerView: 'auto', // розділити на слайди
-      spaceBetween: 20,
-      pagination: {
-        el: '.swiper-pagination3',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next3',
-        prevEl: '.swiper-button-prev3',
-      },
-      breakpoints: {
-        732: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-      },
+        breakpoints: data.breakpoints,
+      });
     });
   }
 });
